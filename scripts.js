1d85c2da90fa9e49f9e17b640121d231e184d268
Stif <stifskere@gmail.com>
2022-02-07 16:32:41 +0100

    Topbar sync

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function() {
     let navbar_height;
-    if (window.scrollY > 600) {
+    if (window.scrollY > 615) {
         document.getElementById('navbar_top').classList.add('fixed-top');
         document.getElementById('navbar_top').classList.remove('topnavborderradius');
         navbar_height = document.querySelector('.navbar').offsetHeight;
