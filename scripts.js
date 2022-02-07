1deb9127a84b96621f4a1922d7fe2affe239827d
Stif <stifskere@gmail.com>
2022-02-07 15:31:22 +0100

    Some changes
    Made it a lil responsive and with border radius

--- scripts.js
+++ scripts.js
@@ -2,10 +2,12 @@ window.addEventListener('scroll', function() {
     let navbar_height;
     if (window.scrollY > 600) {
         document.getElementById('navbar_top').classList.add('fixed-top');
+        document.getElementById('navbar_top').classList.remove('topnavborderradius');
         navbar_height = document.querySelector('.navbar').offsetHeight;
         document.body.style.paddingTop = navbar_height + 'px';
     } else {
         document.getElementById('navbar_top').classList.remove('fixed-top');
+        document.getElementById('navbar_top').classList.add('topnavborderradius');
         document.body.style.paddingTop = '0';
     }
 });
